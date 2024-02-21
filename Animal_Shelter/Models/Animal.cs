cd2d5fe4298f7c888cef804ace6b1fadcfd393a1
Jason Falk <jasonfalk9@gmail.com>
2024-02-21 15:30:08 -0800

    Add class animals with public values for various data.

---
+++ Animal_Shelter/Models/Animal.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Animal_Shelter.Models
+{
+  public class Animals
+  {
+    public int AnimalId { get; set; }
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public string DOA { get; set; }
+    public string Breed { get; set; }
+  }
+}
