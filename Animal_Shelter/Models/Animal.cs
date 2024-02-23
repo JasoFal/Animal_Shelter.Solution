bb1746e460eb576258eca1ef42a090798097973b
Jason Falk <jasonfalk9@gmail.com>
2024-02-23 15:12:25 -0800

    Change DOA datatype from string to DataTime object.

--- Animal_Shelter/Models/Animal.cs
+++ Animal_Shelter/Models/Animal.cs
@@ -1,13 +1,16 @@
-using System.Collections.Generic;
-
 namespace Animal_Shelter.Models
 {
-  public class Animals
+  public class Animal
   {
     public int AnimalId { get; set; }
     public string Type { get; set; }
     public string Name { get; set; }
-    public string DOA { get; set; }
+    public System.DateTime DOA { get; set; }
     public string Breed { get; set; }
+
+    public Animal()
+    {
+      DOA = System.DateTime.Now;
+    }
   }
 }
